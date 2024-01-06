ed8feb8c76e148ce5adb8e9e0e0a72d389d21a58
Mohamed-Ahmed-Esmat <21p0144@eng.asu.edu.eg>
2024-01-06 18:39:01 +0200

    minor style changes

--- src/components/Home Page/Btn.js
+++ src/components/Home Page/Btn.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Btn(props) {
   return (
-    <button className="w-1/6 h-1/10 bg-yellow-500 rounded-lg">
-      <span className="text-purple-800 text-2xl font-bold block w-full h-full">
+    <button className="px-4 py-2 bg-yellow-500 rounded-lg shadow-md hover:bg-yellow-600 active:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-yellow-600 focus:ring-opacity-50">
+      <span className="text-purple-800 text-2xl font-bold">
         {props.text}
       </span>
     </button>
--- src/components/Home Page/LandingBox.js
+++ src/components/Home Page/LandingBox.js
@@ -4,10 +4,19 @@ import { Link } from "react-router-dom";
 
 function LandingBox() {
     return (
-        <div className="relative w-3/4 h-3/4 bg-gradient-to-b from-purple-700 via-purple-600 to-indigo-900 rounded-lg shadow-md">
-            <p className="text-4xl font-bold text-black font-goblin-one break-words">ConCurrency</p>
-            <p className="text-center text-red-600 text-2xl font-bold font-palatino break-words">Make informed financial decisions. Reliable currency conversion, always.</p>
-            <Link to="/currency"><Btn text="Get Started" /></Link>
+        <div className="relative w-3/4 h-3/4 bg-gradient-to-b from-purple-700 via-purple-600 to-indigo-900 rounded-lg shadow-md flex flex-col justify-between items-center p-8">
+            <div>
+                <p className="text-6xl font-bold font-goblin-one break-words text-center">
+                    <span className="text-red-300">C</span>on
+                    <span className="text-red-300">C</span>urrency
+                </p>
+            </div>
+            <div>
+                <p className="text-center text-red-300 text-4xl font-bold font-palatino break-words">Make informed financial decisions. Reliable currency conversion, always.</p>
+            </div>
+            <div>
+                <Link to="/currency"><Btn text="Get Started" /></Link>
+            </div>
         </div>
     );
 }
